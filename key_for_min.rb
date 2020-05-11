9ac8b7ebefe7041233d45963009fe5f48f93b239
Peter Ort <peter@peterort.com>
2020-05-11 19:51:49 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   lowest_value = 0
   lowest_key = nil
-  
+
   name_hash.each do |name, value|
     if lowest_value == 0 || value < lowest_value
       lowest_value = value
@@ -13,6 +13,6 @@ def key_for_min_value(name_hash)
 
   end
   lowest_key
-    
+
 
 end
